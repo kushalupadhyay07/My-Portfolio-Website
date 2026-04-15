23157bd3254b2c5bc3d72807cf3b500e1d277233
kushal upadhyay <kushalupadhyay1818@gmail.com>
2026-04-16 00:12:46 +0530

    Add animated overlay menu with Framer Motion

--- src/components/Overlaymenu.jsx
+++ src/components/Overlaymenu.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import { FiX } from "react-icons/fi";
 
-export default function Overlaymenu() {
+export default function Overlaymenu({ isOpen, isClose }) {
   return (
-    <div>
-      Overlay Menu 
-    </div>
-  )
+    <>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="fixed flex items-center justify-center z-60 inset-0 bg-black/80"
+            initial={{ clipPath: "circle(0% at 50% 50%)" }}
+            animate={{ clipPath: "circle(100% at 50% 50%)" }}
+            transition={{ duration: 1.2, ease: "easeInOut" }}
+            exit={{ clipPath: "circle(0% at 50% 50%)" }}
+            style={{ background: "black", color: "white", padding: "20px" }}
+          >
+            <button
+              className="absolute top-6 right-6 text-3xl cursor-pointer "
+              onClick={isClose}
+            >
+              <FiX />
+            </button>
+
+            <ul className="space-y-6 text-center">
+              {[
+                "Home",
+                "About",
+                "Skills",
+                "Projects",
+                "Experience",
+                "Testimonials",
+                "Contact",
+              ].map((item, index) => {
+                return (
+                  <motion.li
+                    key={item}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.3 + index * 0.1 }}
+                  >
+                    <a
+                      href={`#${item.toLowerCase()}`}
+                      onClick={isClose}
+                      className="text-xl text-white font-semibold hover:text-pink-500"
+                    >
+                      {item}
+                    </a>
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
 }
--- src/components/ParticleBackground.jsx
+++ src/components/ParticleBackground.jsx
@@ -11,7 +11,7 @@ export default function ParticleBackground() {
     canvas.height = window.innerHeight;
 
     const particles = [];
-    const particleCount = 50;
+    const particleCount = 80;
     const colors = ["rgba(255,255,255,0.7)"];
 
     class Particle {
